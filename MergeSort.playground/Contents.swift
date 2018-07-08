85cf416c9576e977787a617f358d9a602ff23533
Sean Lin <seanlin1016@gmail.com>
2018-07-08 17:28:49 +0800

    Merge Sort with Recursion

---
+++ MergeSort.playground/Contents.swift
@@ -0,0 +1,54 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func mergeSort(numbersArray: [Int]) -> [Int] {
+    
+    if numbersArray.count <= 1 { return numbersArray }
+    
+    //split array
+    let leftArray = Array(numbersArray[0..<numbersArray.count/2])
+    let rightArray = Array(numbersArray[numbersArray.count/2..<numbersArray.count])
+    
+    print(leftArray, rightArray)
+    return merge(leftArray: mergeSort(numbersArray: leftArray), rightArray: mergeSort(numbersArray: rightArray))
+}
+
+func merge(leftArray: [Int], rightArray: [Int]) -> [Int]{
+    
+    var mergeArray = [Int]()
+    var left = leftArray
+    var right = rightArray
+
+    print("merge: ", left, right)
+    
+    while left.count > 0 && right.count > 0 {
+        if left[0] < right[0]{
+            mergeArray.append(left[0])
+            left.remove(at: 0)
+        }else if left[0] > right[0]{
+            mergeArray.append(right[0])
+            right.remove(at: 0)
+        }
+    }
+    
+    if left.count != 0 {
+        mergeArray += left
+    }else if right.count != 0 {
+        mergeArray += right
+    }
+    
+    return mergeArray
+}
+
+
+mergeSort(numbersArray: [30, 2, 3, 4, 1, 5, 9, 11])
+
+//merge(leftArray: [2, 3, 7, 19], rightArray: [6, 24])
+
+
+
+
+
+
+
