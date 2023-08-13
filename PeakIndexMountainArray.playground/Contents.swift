2e750e03833f0eba4957a3ca7f1144df55b271dc
Seanlinsanity <seanlin1016@gmail.com>
2023-08-13 09:13:20 +0800

    Peak Index In a Mountain Array

---
+++ PeakIndexMountainArray.playground/Contents.swift
@@ -0,0 +1,60 @@
+import UIKit
+
+/*
+ An array arr is a mountain if the following properties hold:
+
+ arr.length >= 3
+ There exists some i with 0 < i < arr.length - 1 such that:
+ arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+ arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+ Given a mountain array arr, return the index i such that arr[0] < arr[1] < ... < arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
+
+ You must solve it in O(log(arr.length)) time complexity.
+
+  
+
+ Example 1:
+
+ Input: arr = [0,1,0]
+ Output: 1
+ Example 2:
+
+ Input: arr = [0,2,1,0]
+ Output: 1
+ Example 3:
+
+ Input: arr = [0,10,5,2]
+ Output: 1
+  
+
+ Constraints:
+
+ 3 <= arr.length <= 105
+ 0 <= arr[i] <= 106
+ arr is guaranteed to be a mountain array.
+ */
+
+class Solution {
+    func peakIndexInMountainArray(_ arr: [Int]) -> Int {
+        var left = 0
+        var right = arr.count - 1
+        while left <= right {
+            let mid = (left + right) / 2
+            let num = arr[mid]
+            if num > arr[mid + 1] && num > arr[mid - 1] {
+                return mid
+            } else if num > arr[mid + 1] {
+                right = mid - 1
+            } else if num < arr[mid + 1] {
+                left = mid + 1
+            }
+        }
+
+        return 0
+
+    }
+}
+
+Solution().peakIndexInMountainArray([0,1,0])
+Solution().peakIndexInMountainArray([0,2,1,0])
+Solution().peakIndexInMountainArray([0,10,5,2])
