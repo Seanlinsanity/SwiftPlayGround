6885ac5b895bea8cecfa8a87028ef7f4878aa837
Seanlinsanity <seanlin1016@gmail.com>
2023-08-15 18:38:25 +0800

    Merge Sort Updates

--- MergeSort.playground/Contents.swift
+++ MergeSort.playground/Contents.swift
@@ -2,49 +2,54 @@
 
 import UIKit
 
-func mergeSort(numbersArray: [Int]) -> [Int] {
+func mergeSort(_ nums: [Int]) -> [Int] {
+    if nums.count <= 1 {
+        return nums
+    }
     
-    if numbersArray.count <= 1 { return numbersArray }
-    
-    //split array
-    let leftArray = Array(numbersArray[0..<numbersArray.count/2])
-    let rightArray = Array(numbersArray[numbersArray.count/2..<numbersArray.count])
-    
-    print(leftArray, rightArray)
-    
-    //critical point
-    return merge(leftArray: mergeSort(numbersArray: leftArray), rightArray: mergeSort(numbersArray: rightArray))
+    let mid = nums.count / 2
+    let left = Array(nums[0..<mid])
+    let right = Array(nums[mid..<nums.count])
+    return merge(mergeSort(left), mergeSort(right))
 }
 
-func merge(leftArray: [Int], rightArray: [Int]) -> [Int]{
-    
-    var mergeArray = [Int]()
-    var left = leftArray
-    var right = rightArray
 
-    print("merge: ", left, right)
+func merge(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+    var index1 = 0
+    var index2 = 0
     
-    while left.count > 0 && right.count > 0 {
-        if left[0] < right[0]{
-            mergeArray.append(left[0])
-            left.remove(at: 0)
-        }else if left[0] > right[0]{
-            mergeArray.append(right[0])
-            right.remove(at: 0)
+    var merged = [Int]()
+    while index1 < nums1.count && index2 < nums2.count {
+        if nums1[index1] < nums2[index2] {
+            merged.append(nums1[index1])
+            index1 += 1
+        } else {
+            merged.append(nums2[index2])
+            index2 += 1
         }
     }
     
-    return mergeArray + left + right
+    if index1 < nums1.count {
+        for index in index1..<nums1.count {
+            merged.append(nums1[index])
+        }
+    }
+    
+    if index2 < nums2.count {
+        for index in index2..<nums2.count {
+            merged.append(nums2[index])
+        }
+    }
+    
+    return merged
 }
 
-
-mergeSort(numbersArray: [30, 2, 3, 4, 1, 5, 9, 11])
-
-//merge(leftArray: [2, 3, 7, 19], rightArray: [6, 24])
-
-
-
-
+mergeSort([3,1,12,6,7,10,11])
+mergeSort([18,3,0,12,6,7,10,11])
+mergeSort([3,1])
+mergeSort([3,1,2])
+mergeSort([1])
+mergeSort([30,2,3,4,-1,5,9,11])
 
 
 
